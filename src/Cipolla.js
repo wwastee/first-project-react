608862bc94a8e89b5dce06bbb169bb736c19c9dd
fraberto001@gmail.com <fraberto001@gmail.com>
2022-11-17 17:23:20 +0100

    COMMIT 1 ORTAGGI

---
+++ src/Cipolla.js
@@ -0,0 +1,9 @@
+function Cipolla() {
+    return (
+        <div>
+            <img src="https://www.glamourcosmetics.it/images/thumbs/0002474_red-onion_280.jpeg"></img>
+        </div>
+    )
+} 
+
+export default Cipolla;
